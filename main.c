583b6fdf5137fedf0decd4cfb3c9835c211a5443
Christian Joppi <christianjoppi@MacBook-di-Christian.local>
2015-04-30 11:59:51 +0200

    switch server vs client

--- main.c
+++ main.c
@@ -5,11 +5,22 @@
 
 int main(int argc,char** argv)
 {       
-        if(argc!=2)
-        {
-                perror("Error: using %s <server|client>",argv[0]);
-                exit(1);
-        }
-
-        exit(0);
+    if(argc< 2)
+    {
+            perror("Arguments Error: use %s <server|client> --optional_args",argv[0]);
+            exit(1);
+    }
+   
+    #Legge argomento obbligatorio e decide se il processo corrente sarà il server o un client del gioco.
+    switch(argv[1]){
+        case "server": 
+            break;
+        case "client":
+            break;
+        default:
+            perror("Arguments Error: use %s <server|client> --optional_args",argv[0]);
+            exit(1);
+    }
+    
+    exit(0);
 }
